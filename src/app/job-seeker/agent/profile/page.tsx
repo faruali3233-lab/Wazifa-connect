eae0d88418ce6af89efeb0bb91d137ee41626192
faru ali <farualisayyed3233@gmail.com>
2025-08-25 13:21:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/job-seeker/agent/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import AgentProfileForm from "@/components/agent-profile-form";
-
-export default function AgentProfilePage() {
-  return (
-    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8 flex items-center justify-center">
-      <AgentProfileForm />
-    </div>
-  );
-}
